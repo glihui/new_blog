80e28333465175e8cd3b3c86c60754a20d2abe6c
vagrant <vagrant@homestead>
2019-09-22 08:32:42 +0000

    增加 token刷新和删除

--- app/Http/Controllers/Api/AuthorizationsController.php
+++ app/Http/Controllers/Api/AuthorizationsController.php
@@ -29,4 +29,25 @@ class AuthorizationsController extends Controller
                 'expires_in' => \Auth::guard('api')->factory()->getTTL() * 60
             ])->setStatusCode(201);
     }
+
+    protected function respondWithToken($token)
+    {
+        return $this->response->array([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+            'expires_in' => \Auth::guard('api')->factory()->getTTL() * 60
+        ]);
+    }
+
+    public function update()
+    {
+        $token = \Auth::guard('api')->refresh();
+        return $this->respondWithToken($token);
+    }
+
+    public function destroy()
+    {
+        \Auth::guard('api')->logout();
+        return $this->response->noContent();
+    }
 }
